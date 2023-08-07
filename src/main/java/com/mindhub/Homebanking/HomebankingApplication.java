7b6e599be678544b21792501410dd3f33c0ab3d6
gjwenner <gjwenner@gmail.com>
2023-08-06 22:27:29 -0300

    fin de la tarea 1

---
+++ src/main/java/com/mindhub/Homebanking/HomebankingApplication.java
@@ -0,0 +1,27 @@
+package com.mindhub.Homebanking;
+
+import com.mindhub.Homebanking.models.Client;
+import com.mindhub.Homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository) {
+		return (args) ->{
+			Client client = new Client("2236851","Gustavo","Wenner");
+
+			clientRepository.save(client);
+		};
+	}
+
+}
